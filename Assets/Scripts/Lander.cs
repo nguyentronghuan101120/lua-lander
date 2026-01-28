eef8bdf74765cffcebcf00cfc97ac1b885f9aa95
@huannt <nguyentronghuan101120@gmail.com>
2026-01-28 17:23:09 +0700

    Create lander, update movement for lander

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,39 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Lander : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float thrustPower = 20f;
+
+    [SerializeField]
+    private float rotationSpeed = 2f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            rb.AddForce(transform.up * thrustPower);
+        }
+
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            rb.AddTorque(rotationSpeed);
+        }
+        else if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            rb.AddTorque(-rotationSpeed);
+        }
+
+    }
+
+
+}
